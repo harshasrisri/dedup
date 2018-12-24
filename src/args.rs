d4ad3204c3e368f8b39219932b52ae09120a62c0
harshasrisri <harshasrisri@gmail.com>
2018-12-24 08:01:24 -0800

    Parsing arguments using StructOpt

---
+++ src/args.rs
@@ -0,0 +1,26 @@
+use std::path::PathBuf;
+
+/// Program to find duplicate files and take care of them
+#[derive(StructOpt, Debug)]
+#[structopt(name = "dedup")]
+pub struct DedupOpts {
+    /// Activate debug mode
+    #[structopt(short = "d", long = "debug")]
+    pub debug: bool,
+
+    /// Verbose mode (-v, -vv, -vvv, etc.)
+    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    pub verbose: u8,
+
+    /// File containing list of remote files and hashes
+    #[structopt(short = "R", long = "remote-list", parse(from_os_str))]
+    pub remote_list: PathBuf,
+
+    /// Local Path containing files that need to be checked for duplicates
+    #[structopt(short = "l", long = "local-path", parse(from_os_str))]
+    pub local_path: PathBuf,
+
+    /// Type of Hashing algorigthm to use for checksumming. Default is Md5
+    #[structopt(short = "H", long = "hash-algo", default_value = "Md5")]
+    pub hash_algo: String,
+}
