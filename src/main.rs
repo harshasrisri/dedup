d75dfafbd14b3e3ace9af84b03e0c3f4eaf44c8b
harshasrisri <harshasrisri@gmail.com>
2018-12-30 10:58:42 -0800

    Handle remote list coming from stdin

--- src/main.rs
+++ src/main.rs
@@ -32,23 +32,28 @@ fn remove_file (filepath: &Path) -> Result<()> {
     Ok(())
 }
 
-fn dedup_from_set (filepath : &Path, checksums : &HashSet<String>) -> u8 {
-    if filepath.is_dir() == true {
-        return 0;
+fn dedup_from_set (filepath : &Path, checksums : &HashSet<String>) -> bool {
+    match checksum(&filepath) {
+        Ok(chksum) => {
+            let found = match checksums.contains(&chksum) {
+                true  => { let _ = remove_file(filepath); true },
+                false => false ,
+            };
+            found
+        },
+        Err(e) => {
+            eprintln!("Skipping {} - {}", &filepath.to_str().unwrap(), e.to_string());
+            false 
+        }
     }
-
-    let chksum = checksum(&filepath).expect(&format!("Error calculating checksum of {}", &filepath.to_str().unwrap()));
-
-    if checksums.contains(&chksum) {
-        let _ = remove_file(filepath);
-        return 1;
-    }
-    0
 }
 
 fn list_file_to_set (filepath : &Path) -> Result<HashSet<String>> {
     let mut checksums = HashSet::new();
-    let remote = File::open(&filepath.to_str().unwrap())?;
+    let remote : Box<std::io::Read> = match filepath.to_str().unwrap() {
+        "-" => Box::new(std::io::stdin()), 
+        _   => Box::new(File::open(&filepath.to_str().unwrap())?),
+    };
     for line in BufReader::new(remote).lines().filter_map(|result| result.ok()) {
         let hashpath : Vec<&str> = line.splitn(2, ' ').collect();
         checksums.insert(hashpath[0].to_string());
@@ -87,10 +92,13 @@ fn main () {
 
     let mut total_count : usize = 0;
     let mut dup_count : usize = 0;
-    for file in WalkBuilder::new(&local).hidden(false).build().filter_map(|x| x.ok()) {
-        if file.path().is_dir() == false { total_count += 1; }
+
+    for file in WalkBuilder::new(&local).hidden(false).follow_links(false).build().filter_map(|x| x.ok()) {
+        if file.path().is_dir() == true {continue;}
+        total_count += 1; 
         dup_count += dedup_from_set(&file.path(), &checksums) as usize;
     }
+
     println!("{} files processed. {} Duplicates {}", total_count, dup_count, 
              match args.commit {true => "deleted".to_string(), false => "found".to_string()} );
 }
