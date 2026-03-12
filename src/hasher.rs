36e9d98622e2e1efbdc8c0df0d02eae793d144aa
Sriharsha Mucheli <harshasrisri@gmail.com>
2026-03-12 16:54:20 -0700

    Hasher: remove buf chunk iterator and return size along with chksum

--- src/hasher.rs
+++ src/hasher.rs
@@ -3,51 +3,31 @@ use twox_hash::XxHash3_64;
 use std::hash::Hasher;
 use std::path::Path;
 use std::fs::OpenOptions;
-use std::io::Read;
-
-const CHUNK_SIZE: usize = 4096;
-
-pub struct BufChunkIterator<R> {
-    inner: R,
-    chunk_size: usize,
-}
-
-impl<R: Read> Iterator for BufChunkIterator<R> {
-    type Item = Vec<u8>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut buffer = Vec::with_capacity(self.chunk_size);
-        let len = self
-            .inner
-            .by_ref()
-            .take(self.chunk_size as u64)
-            .read_to_end(&mut buffer)
-            .ok();
-        if len != Some(0) {
-            if let Some(len) = len { buffer.truncate(len) }
-            Some(buffer)
-        } else {
-            None
-        }
-    }
-}
+use std::io::{BufRead, BufReader};
 
 pub trait HashFile: AsRef<Path> {
-    fn chksum(&self) -> Result<String>;
+    fn chksum(&self) -> Result<(usize, String)>;
 }
 
 impl<P> HashFile for P
 where 
     P: AsRef<Path>,
 {
-    fn chksum(&self) -> Result<String> {
+    fn chksum(&self) -> Result<(usize, String)> {
         let mut sh = XxHash3_64::with_seed(0xdeadbeef);
+        let capacity = 256 * 1024; // 256 KB
         let inner = OpenOptions::new().read(true).write(false).create(false).open(self)?;
-        let chunk_iter = BufChunkIterator { inner, chunk_size: CHUNK_SIZE };
-        for chunk in chunk_iter {
-            sh.write(&chunk);
+        let mut br = BufReader::with_capacity(capacity, inner);
+        let mut file_size = 0;
+        loop {
+            let buf = br.fill_buf()?;
+            if buf.is_empty() { break; }
+            let buflen = buf.len();
+            file_size += buflen;
+            sh.write(buf);
+            br.consume(buflen);
         }
 
-        Ok(format!("{:X}", sh.finish()))
+        Ok((file_size, format!("{:X}", sh.finish())))
     }
 }
